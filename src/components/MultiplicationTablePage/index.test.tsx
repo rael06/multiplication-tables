a8c49c55aa3eb4fd6b99c7dcee0bd09edc81ef6a
rael06 <rael06@hotmail.fr>
2020-12-23 14:00:55 +0100

    init MultiplicationTablePage tests

---
+++ src/components/MultiplicationTablePage/index.test.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { screen, render } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import MultiplicationTablePage from ".";
+
+describe("<MultiplicationTablePage />", () => {
+  it("Should display Stop button when click on Start button", () => {
+    render(<MultiplicationTablePage />);
+    userEvent.click(screen.getByText("Commencer"));
+    screen.debug();
+  });
+});
